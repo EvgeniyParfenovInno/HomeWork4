663a6492c2284015b6e0d7b0ac76e5f3f57659e8
Парфенов Евгений <esparfenov@vtb.ru>
2024-02-17 22:09:09 +0300

    Добавлен spring-context

--- src/main/java/ru/demo/App.java
+++ src/main/java/ru/demo/App.java
@@ -1,12 +1,14 @@
 package ru.demo;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("ru.demo")
+public class App
 {
     public static void main( String[] args )
     {
+        ApplicationContext context = new AnnotationConfigApplicationContext();
     }
 }
