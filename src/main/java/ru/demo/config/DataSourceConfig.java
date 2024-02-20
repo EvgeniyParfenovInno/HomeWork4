8925b714ab6f36dee85927bdc09a10d174282108
Парфенов Евгений <esparfenov@vtb.ru>
2024-02-20 11:34:22 +0300

    Решение

---
+++ src/main/java/ru/demo/config/DataSourceConfig.java
@@ -0,0 +1,22 @@
+package ru.demo.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+    @Bean
+    public DataSource hikariDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName("org.postgresql.Driver");
+        config.setJdbcUrl("jdbc:postgresql://localhost:5432/postgres");
+        config.setUsername("postgres");
+        config.setPassword("checkPwd");
+
+        return new HikariDataSource(config);
+    }
+}
---
+++ src/main/java/ru/demo/repository/UserDaoImpl.java
@@ -0,0 +1,116 @@
+package ru.demo.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.demo.model.User;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+public class UserDaoImpl implements UserDao {
+
+    private static final String USERNAME_FIELD = "username";
+    private DataSource dataSource;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public Optional<User> findById(Long id) throws SQLException {
+        String sql = "select * from users where id=?";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setLong(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return Optional.of(new User()
+                        .setId(rs.getLong("id"))
+                        .setUserName(rs.getString(USERNAME_FIELD))
+                );
+            }
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public Optional<User> findByName(String name) throws SQLException {
+        String sql = "select * from users where username=?";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, name);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return Optional.of(new User()
+                        .setId(rs.getLong("id"))
+                        .setUserName(rs.getString(USERNAME_FIELD))
+                );
+            }
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public List<User> getAll() throws SQLException {
+        String sql = "select * from users";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+            List<User> users = new LinkedList<>();
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                users.add(new User()
+                        .setId(rs.getLong("id"))
+                        .setUserName(rs.getString(USERNAME_FIELD)));
+            }
+            return users;
+        }
+    }
+
+    @Override
+    public Optional<User> create(String name) throws SQLException {
+        String sql = "insert into users (username) values(?)";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, name);
+            ps.executeUpdate();
+            return findByName(name);
+        }
+    }
+
+    @Override
+    public Optional<User> update(Long id, String name) throws SQLException {
+        String sql = "update users set username=? where id=?";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, name);
+            ps.setLong(2, id);
+            ps.executeUpdate();
+            return findById(id);
+        }
+    }
+
+    @Override
+    public void delete(Long id) throws SQLException {
+        String sql = "delete from users where Id=?";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setLong(1, id);
+            ps.executeUpdate();
+        }
+    }
+
+    @Override
+    public void clear() throws SQLException {
+        String sql = "delete from users";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.executeUpdate();
+        }
+    }
+}
